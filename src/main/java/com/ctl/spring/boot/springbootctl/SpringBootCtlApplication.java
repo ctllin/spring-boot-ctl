362b3b78b170ed62bb5c637480ffe68ada4527a4
林家小院 <guolinit@163.com>
2018-12-17 11:12:46 +0800

    init

---
+++ src/main/java/com/ctl/spring/boot/springbootctl/SpringBootCtlApplication.java
@@ -0,0 +1,14 @@
+package com.ctl.spring.boot.springbootctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCtlApplication.class, args);
+	}
+
+}
+
